8adf2e3ddab20bcc678dc3932a96db6167ceacf0
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-07-15 14:51:44 +0700

    chore: add flowbite plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "node_modules/preline/dist/*.js"
+    "node_modules/preline/dist/*.js",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {},
@@ -12,7 +13,7 @@ module.exports = {
     },
   },
   plugins: [
-    require("preline/plugin")
+    require("preline/plugin", "flowbite/plugin"),
   ],
   darkMode: "class"
 }
