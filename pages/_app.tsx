bb208a45a4feb7f479cf8a9ac459a3b64478c750
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-07-19 11:56:26 +0700

    feat: wrap component with chakra provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import theme from './theme'
+
+import '@fontsource/rubik/400.css'
+import '@fontsource/rubik/600.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
