40e6931ed9e42206a9452662bbe30336cb11c70c
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-07-19 11:57:55 +0700

    feat: add sub-navigation view for desktop

---
+++ components/navigations/desktopSubNav.tsx
@@ -0,0 +1,43 @@
+import { Box, Flex, Link, Stack, Text, Icon, useColorModeValue } from "@chakra-ui/react"
+import { ChevronRightIcon } from "@chakra-ui/icons"
+import { FC } from "react"
+import { NavItem } from "./navType"
+
+const DesktopSubNav: FC<NavItem> = ({ label, href, subLabel }) =>  {
+    return (
+        <Link
+            href={href}
+            role={'group'}
+            display={'block'}
+            p={2}
+            rounded={'md'}
+            _hover={{ bg: useColorModeValue('green.50', 'green.900') }}
+        >
+            <Stack direction={'row'} align={'center'}>
+                <Box>
+                    <Text
+                        transition={'aa .3s ease'}
+                        _groupHover={{ color: 'green.400' }}
+                        fontWeight={500}
+                    >
+                        {label}
+                    </Text>
+                    <Text fontSize={'sm'}>{subLabel}</Text>
+                </Box>
+                <Flex
+                    transition={'all .3s ease'}
+                    transform={'translateX(-10px)'}
+                    opacity={0}
+                    _groupHover={{ opacity: '100%', transform: 'translateX(0)' }}
+                    justify={'flex-end'}
+                    align={'center'}
+                    flex={1}
+                >
+                    <Icon color={'green.400'} w={5} h={5} as={ChevronRightIcon} />
+                </Flex>
+            </Stack>
+        </Link>
+    )
+}
+
+export default DesktopSubNav
