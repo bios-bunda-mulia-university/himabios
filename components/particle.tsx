bd7841ca81f207e85360f708ce88c6b6e87620b8
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-07-20 21:54:15 +0700

    feat: fix particles init

--- components/particle.tsx
+++ components/particle.tsx
@@ -1,45 +1,25 @@
-import { FC, useEffect } from "react"
+import { FC, useCallback, useEffect, useMemo } from "react"
 import Particles from "react-tsparticles"
 import { loadFull } from "tsparticles"
 import { useColorMode, useColorModeValue } from "@chakra-ui/react"
+import type { Engine } from "tsparticles-engine"
+import { ISourceOptions } from "tsparticles-engine"
 
 const ParticlesBackground: FC = () => {
     const { colorMode, toggleColorMode } = useColorMode()
 
-    useEffect(() => {
-        const particlesInit = async (main: any) => {
-            console.log(main)
-            await loadFull(main)
-        }
-    }, [])
-
-    useEffect(() => {
-        const particlesLoaded = (container: any) => {
-            console.log(container)
-        }
-    }, [])
-
-    const particlesInit = async (main: any) => {
-        console.log(main)
-        await loadFull(main)
-    }
-
-    const particlesLoaded = (container: any) => {
-        console.log(container)
-    }
+    // init particles engine
+    const particlesInit = useCallback(async (engine: Engine) => {
+        await loadFull(engine)
+    }, []) 
 
     return (
         <Particles
             id="tsparticles"
             init={particlesInit}
-            loaded={particlesLoaded}
+            // loaded={particlesLoaded}
             options={{
-                // background: {
-                //     color: {
-                //         value: "#151019"
-                //     },
-                // },
-                fpsLimit: 60,
+                fpsLimit: 120,
                 interactivity: {
                     events: {
                         onClick: {
@@ -58,16 +38,16 @@ const ParticlesBackground: FC = () => {
                         },
                         repulse: {
                             distance: 200,
-                            duration: 0.2,
+                            duration: 0.4,
                         },
                     },
                 },
                 particles: {
                     color: {
-                        value: colorMode === 'light' ? "#333333" : "#38F9D7",
+                        value: colorMode === 'light' ? "#43E97B" : "#38F9D7",
                     },
                     links: {
-                        color: colorMode === 'light' ? "#333333" : "#38F9D7",
+                        color: colorMode === 'light' ? "#43E97B" : "#38F9D7",
                         distance: 150,
                         enable: true,
                         opacity: 0.5,
@@ -79,12 +59,10 @@ const ParticlesBackground: FC = () => {
                     move: {
                         direction: "none",
                         enable: true,
-                        outModes: {
-                            default: "bounce",
-                        },
+                        outModes: "bounce",
                         random: false,
                         speed: 3,
-                        straight: true,
+                        straight: false,
                     },
                     number: {
                         density: {
@@ -100,6 +78,7 @@ const ParticlesBackground: FC = () => {
                         type: "triangle",
                     },
                     size: {
+                        random: true,
                         value: { min: 1, max: 5 },
                     },
                 },
