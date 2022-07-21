f1861f6631501feb66e231bf62ec0a383aa46674
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-07-21 11:50:56 +0700

    refactor: using local variable to pass into options props

--- components/particle.tsx
+++ components/particle.tsx
@@ -3,6 +3,7 @@ import Particles from "react-tsparticles"
 import { loadFull } from "tsparticles"
 import { useColorMode } from "@chakra-ui/react"
 import type { Engine } from "tsparticles-engine"
+import { ISourceOptions } from "tsparticles-engine"
 
 type Props = {
     id: string
@@ -10,6 +11,78 @@ type Props = {
 
 const ParticlesBackground: FC<Props> = ({ id }) => {
     const { colorMode } = useColorMode()
+
+    const options: ISourceOptions = useMemo(() => {
+        return {
+            fpsLimit: 120,
+            interactivity: {
+                events: {
+                    onClick: {
+                        enable: true,
+                        mode: "push",
+                    },
+                    onHover: {
+                        enable: true,
+                        mode: "repulse",
+                    },
+                    resize: true,
+                },
+                modes: {
+                    push: {
+                        quantity: 5,
+                    },
+                    repulse: {
+                        distance: 200,
+                        duration: 0.4,
+                    },
+                },
+            },
+            particles: {
+                color: {
+                    value: colorMode === "light" ? "#1F8843" : "#43E97B",
+                },
+                links: {
+                    color: colorMode === "light" ? "#1F8843" : "#43E97B",
+                    distance: 150,
+                    enable: true,
+                    opacity: 0.5,
+                    width: 1,
+                },
+                collisions: {
+                    enable: true,
+                },
+                move: {
+                    direction: "none",
+                    enable: true,
+                    outModes: "bounce",
+                    random: false,
+                    speed: 3,
+                    straight: false,
+                },
+                number: {
+                    density: {
+                        enable: true,
+                        area: 800,
+                    },
+                    value: 80,
+                },
+                opacity: {
+                    value: 0.5,
+                },
+                shape: {
+                    type: "triangle",
+                },
+                size: {
+                    random: true,
+                    value: {
+                        min: 1,
+                        max: 5,
+                    },
+                },
+                detectRetina: true,
+            },
+        }
+    }, [colorMode])
 
     // init particles engine
     const particlesInit = useCallback(async (engine: Engine) => {
@@ -21,72 +94,7 @@ const ParticlesBackground: FC<Props> = ({ id }) => {
             id={id}
             init={particlesInit}
             // loaded={particlesLoaded}
-            options={{
-                fpsLimit: 120,
-                interactivity: {
-                    events: {
-                        onClick: {
-                            enable: true,
-                            mode: "push",
-                        },
-                        onHover: {
-                            enable: true,
-                            mode: "repulse",
-                        },
-                        resize: true,
-                    },
-                    modes: {
-                        push: {
-                            quantity: 5,
-                        },
-                        repulse: {
-                            distance: 200,
-                            duration: 0.4,
-                        },
-                    },
-                },
-                particles: {
-                    color: {
-                        value: colorMode === 'light' ? "#1F8843" : "#43E97B",
-                    },
-                    links: {
-                        color: colorMode === 'light' ? "#1F8843" : "#43E97B",
-                        distance: 150,
-                        enable: true,
-                        opacity: 0.5,
-                        width: 1,
-                    },
-                    collisions: {
-                        enable: true,
-                    },
-                    move: {
-                        direction: "none",
-                        enable: true,
-                        outModes: "bounce",
-                        random: false,
-                        speed: 3,
-                        straight: false,
-                    },
-                    number: {
-                        density: {
-                            enable: true,
-                            area: 800,
-                        },
-                        value: 80,
-                    },
-                    opacity: {
-                        value: 0.5,
-                    },
-                    shape: {
-                        type: "triangle",
-                    },
-                    size: {
-                        random: true,
-                        value: { min: 1, max: 5 },
-                    },
-                },
-                detectRetina: true,
-            }}
+            options={options}
         />
     )
 }
