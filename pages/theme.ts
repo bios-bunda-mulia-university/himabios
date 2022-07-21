ca6446bc7e347762eb458551e3d538dec965c047
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-07-21 10:59:45 +0700

    chore: update extend color palatte

--- pages/theme.ts
+++ pages/theme.ts
@@ -14,15 +14,27 @@ const theme = extendTheme({
     },
     colors: {
         transparent: 'transparent',
-        darkMode: '#151019',
-        lightMode: '#EEEEEE'
+        green: {
+            ligtMain: '#1F8843',
+            lightSurface: '#D2E7D9',
+            lightBorder: '#B4D7C0',
+            lightHover: '#1A7138',
+            lightPressed: '#0F4421',
+            lightFocus: '#D9FBE5', // opacity: 0.2
+            darkMain: '#43E97B',
+            darkSurface: '#D9FBE5',
+            darkBorder: '#C0F8D3',
+            darkHover: '#38C266',
+            darkPressed: '#21743D',
+            darkFocus: '#43E97B', // opacity: 0.2
+        }
     },
     semanticTokens: {
         colors: {
             // Change the colors of chakra dark/light mode
             "chakra-body-bg": {
-                _light: "lightMode",
-                _dark: "darkMode",
+                // _light: yourColor,
+                _dark: '#121212',
             }
         }
     }
