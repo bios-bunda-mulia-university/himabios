22dcd4377802e11678d8577c62f1e16e4394d8a8
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-08-01 16:11:38 +0700

    chore: delete unused code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import Header from '../components/header'
 import Line from '../components/particles/line'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
