d477e530ad5738cd1ac2d074c51a0dc8b1823981
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-08-01 13:39:37 +0700

    chore: extend tailwind theme configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,38 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      'primary': {
+        light: {
+          main: '#1F8843',
+          surface: '#D2E7D9',
+          border: '#B4D7C0',
+          hover: '#1A7138',
+          pressed: '#0F4421',
+          focus: '#D9FBE5'
+        },
+        dark: {
+          main: '#43E97B',
+          surface: '#D9FBE5',
+          border: '#C0F8D3',
+          hover: '#38C266',
+          pressed: '#21743D',
+          focus: '#43E97B'
+        },
+      },
+    },
+    extend: {
+      opacity: {
+        '08': '0.08',
+        '38': '0.38',
+      },
+      backdropBlur: {
+        5: '5px',
+      },
+      dropShadow: {
+        'custom': '0 10px 30px rgba(0, 0, 0, 0.05)',
+      }
+    },
     fontFamily: {
       'inter': ['Inter', 'sans-serif'],
     },
