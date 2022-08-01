0658f8be3e736cfb9f8fa87c56256b3eb9b77e49
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-08-01 14:56:20 +0700

    feat: add navigation for desktop view

---
+++ components/navigations/desktopView.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link"
+import { FC } from "react"
+import { NAV_ITEMS } from "./itemsList"
+
+const DesktopView: FC = () => {
+    return (
+        <div className="flex flex-row justify-start items-center px-16">
+            {NAV_ITEMS.map((navItem) => (
+                <Link key={navItem.label} href={navItem.label}>
+                    <a key={navItem.label} className="px-3 font-medium font-inter text-lg text-neutral-600 transition ease-in-out hover:text-primary-light-hover hover:-translate-y-1">
+                        {navItem.label}
+                    </a>
+                </Link>
+            ))}
+        </div>
+    )
+}
+
+export default DesktopView
