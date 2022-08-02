0bda91e6144e6fcebc76e5909330f1b5f3c69963
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-08-02 15:29:39 +0700

    style: change nav position to fixed

--- components/header.tsx
+++ components/header.tsx
@@ -7,7 +7,7 @@ const Header: FC = () => {
     const imgPath = "https://bios-space.sfo3.digitaloceanspaces.com/logo-bios.png"
 
     return (
-        <nav className="sticky top-0 flex flex-row px-12 py-5 bg-gradient-to-br from-white/38 to-white/08 z-10 backdrop-blur-5 drop-shadow-custom">
+        <nav className="fixed top-0 flex flex-row px-12 py-5 bg-gradient-to-br from-white/38 to-white/08 z-10 backdrop-blur-5 drop-shadow-custom">
             <Logo imgUrl={imgPath} text="BIOS" sizeHeight={50} sizeWidth={50} />
             <MenuDesktopView />
             <IconNav />
