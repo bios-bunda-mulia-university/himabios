812d7a40cdffe16e59bf1d85dcdea596b622db13
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-08-02 15:29:45 +0700

    feat: add hero

---
+++ components/body/hero.tsx
@@ -0,0 +1,18 @@
+import { FC } from "react"
+
+const Hero: FC = () => {
+    return (
+        <main className="flex flex-col items-center justify-center h-screen space-y-14">
+            <h1 className="text-neutral-900 font-inter font-bold text-6xl z-10">
+                <span className="text-transparent bg-clip-text bg-gradient-to-br from-carribean-green to-turquoise">Kesulitan</span> Memahami Materi?
+            </h1>
+            <p className="text-black font-inter font-medium text-xl">
+                Merasakan hal yang sama seperti mereka?
+                <br />
+                Yuk klik tombol dibawah ini untuk memulai
+            </p>
+        </main>
+    )
+}
+
+export default Hero
