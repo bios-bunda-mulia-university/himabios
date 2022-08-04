351a04f35be7864a533b35fc726265246f0f5bd7
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-08-04 19:53:06 +0700

    style: update z index and change navbar to sticky

--- components/header.tsx
+++ components/header.tsx
@@ -7,11 +7,12 @@ const Header: FC = () => {
     const imgPath = "https://bios-space.sfo3.digitaloceanspaces.com/logo-bios.png"
 
     return (
-        <nav className="fixed top-0 flex flex-row px-12 py-5 bg-gradient-to-br from-white/38 to-white/08 z-10 backdrop-blur-5 drop-shadow-custom">
+        <nav className="sticky top-0 flex flex-row px-12 py-5 bg-gradient-to-br from-white/38 to-white/08 z-20 backdrop-blur-5 drop-shadow-custom">
             <Logo imgUrl={imgPath} text="BIOS" sizeHeight={50} sizeWidth={50} />
             <MenuDesktopView />
             <IconNav />
         </nav>
+
     )
 }
 
