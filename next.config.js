165866f0422449f5e32af1d4863b76afd726f9d1
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-08-10 11:24:05 +0700

    feat: add mdx config for next

--- next.config.js
+++ next.config.js
@@ -7,4 +7,18 @@ const nextConfig = {
   },
 }
 
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    providerImportSource: '@mdx-js/react'
+  }
+})
+
 module.exports = nextConfig
+module.exports = withMDX({
+  // Append the default value with md extensions
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx']
+})
