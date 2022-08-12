ec64ce78a573cff952ebe6b31113a5cc048a57e5
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-08-12 17:23:33 +0700

    chore: remove to footer

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -2,7 +2,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
 import { faInstagram, faTiktok, faGithub } from "@fortawesome/free-brands-svg-icons"
 import { FC } from "react"
-import Logo from "./logo"
+import Logo from "../logo"
 
 const Footer: FC = () => {
     const imgPath = "https://bios-space.sfo3.digitaloceanspaces.com/logo-bios.png"
