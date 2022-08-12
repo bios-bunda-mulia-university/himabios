0cefcf211542e0fd4e3934bbc08e3fac0d56d2dc
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-08-12 17:43:20 +0700

    feat: add footer icon lists

---
+++ components/footer/footerIconLists.ts
@@ -0,0 +1,30 @@
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
+import { faInstagram, faTiktok, faGithub } from "@fortawesome/free-brands-svg-icons"
+import { FooterTypes } from "./types";
+
+export const FOOTER_ITEMS: FooterTypes[] = [
+    {
+        href: 'https://instagram.com/bios_ubm',
+        target: '_blank',
+        rel: 'noopener noreferrer',
+        icon: faInstagram
+    },
+    {
+        href: 'https://tiktok.com/@bios_ubm',
+        target: '_blank',
+        rel: 'noopener noreferrer',
+        icon: faTiktok
+    },
+    {
+        href: 'https://github.com/bios-bunda-mulia-university',
+        target: '_blank',
+        rel: 'noopener noreferrer',
+        icon: faGithub
+    },
+    {
+        href: 'mailto:himaitubm@gmail.com',
+        target: '_blank',
+        rel: 'noopener noreferrer',
+        icon: faEnvelope
+    }
+]
