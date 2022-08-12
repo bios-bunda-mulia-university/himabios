59508a0309445495b859ae62f52c773dd753b064
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-08-12 17:44:48 +0700

    refactor: use icon map

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -3,6 +3,7 @@ import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
 import { faInstagram, faTiktok, faGithub } from "@fortawesome/free-brands-svg-icons"
 import { FC } from "react"
 import Logo from "../logo"
+import { FOOTER_ITEMS } from "./footerIconLists"
 
 const Footer: FC = () => {
     const imgPath = "https://bios-space.sfo3.digitaloceanspaces.com/logo-bios.png"
@@ -12,18 +13,11 @@ const Footer: FC = () => {
             <Logo imgUrl={imgPath} text="BIOS" sizeHeight={50} sizeWidth={50} />
             <p className="text-neutral-800 font-inter font-normal text-md">BIOS adalah organisasi himpunan<br /> mahasiswa Universitas Bunda Mulia.</p>
             <div className="space-x-5">
-                <a href="https://instagram.com/bios_ubm" target="_blank" rel="noopener noreferrer">
-                    <FontAwesomeIcon icon={faInstagram} color="#404040" size="lg" className="hover:cursor-pointer" />
-                </a>
-                <a href="https://tiktok.com/@bios_ubm" target="_blank" rel="noopener noreferrer">
-                    <FontAwesomeIcon icon={faTiktok} color="#404040" size="lg" className="hover:cursor-pointer" />
-                </a>
-                <a href="https://github.com/bios-bunda-mulia-university" target="_blank" rel="noopener noreferrer">
-                    <FontAwesomeIcon icon={faGithub} color="#404040" size="lg" className="hover:cursor-pointer" />
-                </a>
-                <a href="mailto:himaitubm@gmail.com">
-                    <FontAwesomeIcon icon={faEnvelope} color="#404040" size="lg" className="hover:cursor-pointer" />
-                </a>
+                {FOOTER_ITEMS.map((footerIcon) => (
+                    <a key={footerIcon.href} href={footerIcon.href} target={footerIcon.target} rel={footerIcon.rel}>
+                        <FontAwesomeIcon icon={footerIcon.icon} color="#404040" size="lg" className="hover:cursor-pointer" />
+                    </a>
+                ))}
             </div>
             <p className="font-inter font-thin text-neutral-500 text-sm">Copyright &copy; 2022 BIOS. All rights reserved.</p>
         </footer>
