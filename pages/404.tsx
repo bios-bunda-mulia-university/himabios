9ece62c8c6e92cdfb4b361d735918b089d3c9900
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-08-15 13:21:24 +0700

    ui: properly layout

--- pages/404.tsx
+++ pages/404.tsx
@@ -9,9 +9,10 @@ const NotFound = () => {
     return (
         <div className="flex flex-col items-center justify-center bg-black h-screen w-screen">
             <Image src={imgUrl} alt="404 Illustration" width={500} height={500} className="z-10" />
-            <h1 className="text-white font-inter font-bold text-5xl">Oopss.. Halaman Tidak Ditemukan</h1>
+            <h1 className="text-neutral-50 font-inter font-bold text-5xl">Oopss.. Halaman Tidak Ditemukan</h1>
             <Link href="/">
-                <a className="bg-primary-light-main p-4 font-inter font-medium text-base text-white rounded-md z-10">
+                
+                <a className="bg-primary-light-main p-4 font-inter font-normal text-base mt-10 tracking-wider text-neutral-50 rounded-md z-10">
                     Kembali ke halaman utama
                 </a>
             </Link>
