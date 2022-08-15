50ff591c3ed3bf34165e387b4c4bfa6ee3953491
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-08-15 21:01:44 +0700

    feat: add icon to CTA button

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import Link from "next/link";
 import { ReactElement } from "react";
 import Stars from "../components/particles/stars";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 const NotFound = () => {
     const imgUrl = "https://bios-space.sfo3.digitaloceanspaces.com/404%203d.svg"
@@ -11,8 +13,8 @@ const NotFound = () => {
             <Image src={imgUrl} alt="404 Illustration" width={500} height={500} className="z-10" />
             <h1 className="text-neutral-50 font-inter font-bold text-5xl">Oopss.. Halaman Tidak Ditemukan</h1>
             <Link href="/">
-                
                 <a className="bg-primary-light-main p-4 font-inter font-normal text-base mt-10 tracking-wider text-neutral-50 rounded-md z-10">
+                    <FontAwesomeIcon icon={faArrowLeft} className="mr-4 text-neutral-50 hover:animate-bounce" size="lg" />
                     Kembali ke halaman utama
                 </a>
             </Link>
