d96b01a73e5fe7d6906c25598d074a6099e8dfe6
= <daylanaja@gmail.com>
2022-08-16 23:18:59 +0700

    chore: add space

--- components/sections/whyBios.tsx
+++ components/sections/whyBios.tsx
@@ -5,8 +5,7 @@ const WhyBios: FC = () => {
 		<div className='flex flex-row items-center justify-between text-black max-w-5xl mx-auto gap-5 z-10'>
 			<article className='space-y-8 z-10'>
 				<h2 className='text-neutral-900 dark:text-neutral-100 text-4xl font-bold font-inter z-10'>
-					Kenapa sih harus pakai
-					<span className='text-transparent bg-clip-text bg-gradient-to-br from-carribeangreen dark:from-primary-dark-main to-turquoise dark:to-aquamarine'>BIOS</span>?
+					Kenapa sih harus pakai <span className='text-transparent bg-clip-text bg-gradient-to-br from-carribeangreen dark:from-primary-dark-main to-turquoise dark:to-aquamarine'>BIOS</span>?
 				</h2>
 				<div className='font-inter z-10 text-neutral-900 dark:text-neutral-200'>Apakah kamu mengalami gejala seperti berikut?</div>
 				<ol className='font-inter z-10 text-neutral-900 dark:text-neutral-200'>
