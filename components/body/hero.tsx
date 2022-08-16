975e8dda0dfd6d3e65ac212279a6dc59f81e328c
= <daylanaja@gmail.com>
2022-08-16 23:27:48 +0700

    chore: add space and optimize code styling

--- components/body/hero.tsx
+++ components/body/hero.tsx
@@ -6,10 +6,7 @@ const Hero: FC = () => {
 	return (
 		<div className='flex flex-col items-center justify-center h-screen space-y-14'>
 			<h1 className='text-neutral-900 dark:text-neutral-100 font-inter font-bold text-6xl z-10'>
-				<span className='text-transparent bg-clip-text bg-gradient-to-br from-carribeangreen dark:from-primary-dark-main to-turquoise dark:to-aquamarine'>
-					Kesulitan
-				</span>
-				Memahami Materi?
+				<span className='text-transparent bg-clip-text bg-gradient-to-br from-carribeangreen dark:from-primary-dark-main to-turquoise dark:to-aquamarine'>Kesulitan</span> Memahami Materi?
 			</h1>
 			<p className='text-black dark:text-neutral-200 font-inter font-medium text-xl'>
 				Merasakan hal yang sama seperti mereka?
@@ -17,13 +14,8 @@ const Hero: FC = () => {
 				Yuk klik tombol dibawah ini untuk memulai
 			</p>
 			<button className='bg-gradient-to-br from-pastelgreen dark:from-primary-dark-main to-turquoise dark:to-aquamarine z-10 font-inter font-semibold text-xl space-x-4 p-5 rounded-lg'>
-				<FontAwesomeIcon
-					icon={faRocket}
-					className='space-x-4 text-neutral-100 dark:text-neutral-900'
-				/>
-				<span className='text-neutral-100 dark:text-neutral-900'>
-					Ayo Mulai Sekarang
-				</span>
+				<FontAwesomeIcon icon={faRocket} className='space-x-4 text-neutral-100 dark:text-neutral-900' />
+				<span className='text-neutral-100 dark:text-neutral-900'>Ayo Mulai Sekarang</span>
 			</button>
 		</div>
 	)
